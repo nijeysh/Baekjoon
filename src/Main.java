79c847e7b3cbf92085a1989eb5d868ed4ef3cb12
nijeysh <yejin6521@naver.com>
2024-10-20 22:42:16 +0900

    temp commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new BasicMath().baekjoon2292();
+        new BasicMath().baekjoon1193();
     }
 }
--- src/baekjoon/BasicMath.java
+++ src/baekjoon/BasicMath.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BasicMath {
@@ -103,8 +104,6 @@ public class BasicMath {
      *
      */
     public void baekjoon2292() throws IOException {
-        // 1
-        // 2, 3, 4, 5, 6, 7
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         long count = 1;
@@ -122,5 +121,36 @@ public class BasicMath {
             i++;
         }
         System.out.print(i + 1);
+    }
+
+
+    /**
+     * 분수 찾기
+     * 단위는 4
+     */
+    public void baekjoon1193() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[count][count];
+        int num = 0;
+        int row = 0;
+        int column = 0;
+        Boolean topdown = false;
+
+        // 1 -> (0,0) / 2-> (0,1) (1, 0) / 3-> (2,0) (1,1) (0, 2) / (0, 3) (1, 2) (2, 1) (3, 0) / (4, 0) ...
+        while (count >= num) {
+            if (topdown) {
+                matrix[row][column] = 1;
+                if (row == 0) {
+                    break;
+                }
+            } else {
+                matrix[row][column] = 1;
+                if (column == 0) {
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
