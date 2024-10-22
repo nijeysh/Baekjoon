f304df9d090d2452ee439853ba53ad11b1e9f3e8
yejin <nijey@infotech.co.kr>
2024-10-22 13:48:36 +0900

    temporary code commit for test purposes

--- src/baekjoon/TSS.java
+++ src/baekjoon/TSS.java
@@ -7,4 +7,23 @@ import java.util.StringTokenizer;
 
 public class TSS {
 
+    /**
+     * 네 번째 좌표 배열 활용
+     */
+    public void baekjoon3009() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] x = new int[3];
+        int[] y = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            String[] input = br.readLine().split(" ");
+            x[i] = Integer.parseInt(input[0]);
+            y[i] = Integer.parseInt(input[1]);
+        }
+
+        int fourthX = (x[0] == x[1]) ? x[2] : (x[0] == x[2] ? x[1] : x[0]);
+        int fourthY = (y[0] == y[1]) ? y[2] : (y[0] == y[2] ? y[1] : y[0]);
+
+        System.out.println(fourthX + " " + fourthY);
+    }
 }
