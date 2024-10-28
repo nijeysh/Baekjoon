fd47a0f20abb8bbfe5e2b74ae28ba35bd6c85b92
nijeysh <yejin6521@naver.com>
2024-10-28 21:02:53 +0900

    점근적 표기

--- src/baekjoon/timecomplexity/Algorithm1.java
+++ src/baekjoon/timecomplexity/Algorithm1.java
@@ -151,36 +151,73 @@ public class Algorithm1 {
     }
 
     // 수정예정
+    /**
+     *
+     * O(g(n)) = {f(n) | 모든 n ≥ n0에 대하여 f(n) ≤ c × g(n)인 양의 상수 c와 n0가 존재한다.}
+     * f(n) = a1n + a0,
+     * (0 ≤ |ai| ≤ 100) => 음수가 가능하다
+     * (1 ≤ c ≤ 100)
+     * (1 ≤ n0 ≤ 100)
+     * f(n), c, n0가 O(n) 정의를 만족하면 1, 아니면 0을 출력한다.
+     *
+     *
+     * 7 7
+     * 8
+     * 1
+     * => 0
+     * f(n) = 7n + 7, g(n) = n, c = 8, n0 = 1이다. f(1) = 14, c × g(1) = 8이므로 O(n) 정의를 만족하지 못한다.
+     *
+     *
+     * 7 7
+     * 8
+     * 10
+     * => 1
+     * f(n) = 7n + 7, g(n) = n, c = 8, n0 = 10이다. 모든 n ≥ 10에 대하여 7n + 7 ≤ 8n 이므로 O(n) 정의를 만족한다.
+     */
     public void baekjoon24313() throws IOException {
-        // f(n) = a1 * n + a2;
+        // f(n) = a1 * n + a0;
         // n => n >= n0,  f(n) <= c * g(n) 안에 c와 n0이 존재
         // n의 조건 ..
-        int a1 = read();    // 7
-        int a0 = read();    // 7
-        int n;
-        int c = read();     // 8
-        int n0 = read();    // 1
+        int a1 = read();    // 4
+        int a0 = read();    // -2
+        int c = read();     // 4
+        int n0 = read();    // 2
+        boolean bigO = true;
 
-        n = n0;
-        boolean exist = check(a1, n, a0, c);
-        System.out.print(exist ? 1 : 0);
-    }
-    private static boolean check(int a1, int n, int a0, int c) {
-        for (int i = n; i <= 100; i++) {
-            if ((a1 * i) + a0 > c * i) {
-                return false;
+        // (c - a1)n >= a0
+        // 음수일때
+        if (c < a1) {
+            bigO = false;
+
+        // 0일 때
+        } else if (c == a1) {
+            // a0는 음수가 가능하기 때문에
+            if (a0 > 0) {
+                bigO = false;
+            }
+
+        // 양수일 때: if (c > a1)
+        } else {
+            if ((c - a1) * n0 < a0) {
+                bigO = false;
             }
         }
-        return true;
+        System.out.print(bigO ? 1 : 0);
     }
 
     private static int read() throws IOException {
         int n;
         int result = 0;
+        int operation = 1;
         while (true) {
             n = System.in.read();
+            if (n == '-') {
+                operation = -1;
+                continue;
+            }
+
             if (n < '0' || n > '9') {
-                return result;
+                return result * operation;
             }
 
             result *= 10;
