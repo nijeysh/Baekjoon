5252f336c0b404b03cadabd5b45f1ccc5dd355ca
nijeysh <yejin6521@naver.com>
2024-11-10 23:55:19 +0900

    merge sort

--- src/baekjoon/sort/Algorithm.java
+++ src/baekjoon/sort/Algorithm.java
@@ -1,6 +1,7 @@
 package baekjoon.sort;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Algorithm {
 
@@ -10,14 +11,67 @@ public class Algorithm {
      * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
      * 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
      */
+
+    private static int[] sortedArr;
     public void baekjoon2750() throws IOException {
-        int n = read();
         // 음수도 있음
         // 선택, 삽입, 버블 등
         // 퀵 혹은 병합
+        int n = read();
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            
+            arr[i] = read();
         }
+
+        sortedArr = new int[n];
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("arr : " + Arrays.toString(sortedArr));
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int l = left;
+        int m = mid + 1;
+        int idx = left;
+
+        while (l <= mid && m <= right) {
+            if (arr[l] < arr[m]) {
+                sortedArr[idx] = arr[l];
+                l++;
+            } else {
+                sortedArr[idx] = arr[m];
+                m++;
+            }
+            idx++;
+        }
+
+        if (l > mid) {
+            for (int i = m; i <= right; i++) {
+                sortedArr[idx] = arr[i];
+                idx++;
+            }
+        } else {
+            for (int i = l; i <= mid; i++) {
+                sortedArr[idx] = arr[i];
+                idx++;
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            arr[i] = sortedArr[i];
+        }
+
     }
 
     private int read() throws IOException {
