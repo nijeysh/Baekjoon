2f3393048b7f627b8d283ed89d51fdde43f83209
yejin <nijey@infotech.co.kr>
2024-11-20 13:03:52 +0900

    가지고 있는 카드 개수

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new DataStructure().baekjoon1620();
+        new DataStructure().baekjoon10816();
     }
 }
 
