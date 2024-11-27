5664c78b3f960d61ef12085ddff6469a2fa52f9c
nijeysh <yejin6521@naver.com>
2024-11-27 23:35:25 +0900

    기약분수

--- src/baekjoon/numbertheory/DivisorMultiplierPrime2.java
+++ src/baekjoon/numbertheory/DivisorMultiplierPrime2.java
@@ -52,6 +52,28 @@ public class DivisorMultiplierPrime2 {
         System.out.println(min);
     }
 
+    /**
+     * 분수 A/B는 분자가 A, 분모가 B인 분수를 의미한다. A와 B는 모두 자연수라고 하자.
+     *
+     * 두 분수의 합 또한 분수로 표현할 수 있다.
+     * 두 분수가 주어졌을 때, 그 합을 기약분수의 형태로 구하는 프로그램을 작성하시오. 기약분수란 더 이상 약분되지 않는 분수를 의미한다.
+     */
+    public void baekjoon1735() throws IOException {
+        int a = read();
+        int b = read();
+        int c = read();
+        int d = read();
+        StringBuilder sb = new StringBuilder();
+        int gcd = gcd(b, d);
+//        int numerator = ((d / gcd) * a) + ((b / gcd) * c);
+        int numerator = (a * d + c * b) / gcd;
+        int denominator = b * d / gcd;
+        int gcd2 = gcd(numerator, denominator);
+        sb.append(numerator / gcd2).append(" ").append(denominator / gcd2);
+
+        System.out.println(sb);
+    }
+
     // 최대 공약수
     private static int gcd(int a, int b) {
         while (b != 0) {
@@ -65,7 +87,7 @@ public class DivisorMultiplierPrime2 {
 //        return gcd(b, a % b);
     }
 
-    // 최소 공배수
+    // 최소 공배수 = (두 자연수의 곱 / 최대공약수)
     private static int lcm(int a, int b) {
         int gcd = gcd(a, b);
         return a * b / gcd;
