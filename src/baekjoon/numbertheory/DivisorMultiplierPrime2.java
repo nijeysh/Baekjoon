725762279fe2bdafdc8d526a054c3ae40f6d2d2d
nijeysh <yejin6521@naver.com>
2024-11-30 19:51:40 +0900

    가로수

--- src/baekjoon/numbertheory/DivisorMultiplierPrime2.java
+++ src/baekjoon/numbertheory/DivisorMultiplierPrime2.java
@@ -1,7 +1,6 @@
 package baekjoon.numbertheory;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class DivisorMultiplierPrime2 {
     public void baekjoon1934() throws IOException {
@@ -75,40 +74,28 @@ public class DivisorMultiplierPrime2 {
         System.out.println(sb);
     }
 
+    /**
+     * 직선으로 되어있는 도로의 한 편에 가로수가 임의의 간격으로 심어져있다. KOI 시에서는 가로수들이 모두 같은 간격이 되도록 가로수를 추가로 심는 사업을 추진하고 있다. KOI 시에서는 예산문제로 가능한 한 가장 적은 수의 나무를 심고 싶다.
+     *
+     * 편의상 가로수의 위치는 기준점으로 부터 떨어져 있는 거리로 표현되며, 가로수의 위치는 모두 양의 정수이다.
+     * 예를 들어, 가로수가 (1, 3, 7, 13)의 위치에 있다면 (5, 9, 11)의 위치에 가로수를 더 심으면 모든 가로수들의 간격이 같게 된다.
+     * 또한, 가로수가 (2, 6, 12, 18)에 있다면 (4, 8, 10, 14, 16)에 가로수를 더 심어야 한다.
+     *
+     * 심어져 있는 가로수의 위치가 주어질 때, 모든 가로수가 같은 간격이 되도록 새로 심어야 하는 가로수의 최소수를 구하는 프로그램을 작성하라.
+     * 단, 추가되는 나무는 기존의 나무들 사이에만 심을 수 있다.
+     */
     public void baekjoon2485() throws IOException {
         int n = read();
         int[] arr = new int[n];
         arr[0] = read();
         arr[1] = read();
-        int gap = arr[1] - arr[0];
-        int gcd = gcd(arr[1], arr[0]);
-        int count = 0;
+        int gcd = arr[1] - arr[0];
 
         for (int i = 2; i < n; i++) {
             arr[i] = read();
-            gcd = gcd(arr[i], gcd);
-            gap = arr[i] - arr[i - 1];
+            gcd = gcd(arr[i] - arr[i - 1], gcd);
         }
-
-        System.out.println(gcd);
-        System.out.println((arr[n - 1] - arr[0]) / gcd);
-        System.out.println(n - 1);
         System.out.print(((arr[n - 1] - arr[0]) / gcd) - (n - 1));
-
-        // 이거 맞는데..
-//        System.out.println(gcd(2, gcd(6, gcd(12, 18))));
-//        int current = arr[0] + gcd;
-//        int index = 1;
-//        while (index < arr.length) {
-//            if (arr[index] == current) {
-//                index++;
-//            } else {
-//                count++;
-//            }
-//            current += gcd;
-//        }
-//        System.out.println(gcd);
-//        System.out.println(count);
     }
 
     // 최대 공약수
