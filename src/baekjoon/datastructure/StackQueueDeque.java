a5e7c0384a56b08ea5b7d88f03402729ea29ae56
nijey <yejin6521@naver.com>
2024-12-09 13:00:28 +0900

    no message

--- src/baekjoon/datastructure/StackQueueDeque.java
+++ src/baekjoon/datastructure/StackQueueDeque.java
@@ -1,4 +1,22 @@
 package baekjoon.datastructure;
 
+import java.io.IOException;
+
 public class StackQueueDeque {
+
+    /**
+     *
+     * 정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+     *
+     * 명령은 총 다섯 가지이다.
+     *
+     * 1 X: 정수 X를 스택에 넣는다. (1 ≤ X ≤ 100,000)
+     * 2: 스택에 정수가 있다면 맨 위의 정수를 빼고 출력한다. 없다면 -1을 대신 출력한다.
+     * 3: 스택에 들어있는 정수의 개수를 출력한다.
+     * 4: 스택이 비어있으면 1, 아니면 0을 출력한다.
+     * 5: 스택에 정수가 있다면 맨 위의 정수를 출력한다. 없다면 -1을 대신 출력한다.
+     *
+     */
+    public void baekjoon28278() throws IOException {
+    }
 }
