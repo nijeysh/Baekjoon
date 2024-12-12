1ee8de65e6933f605f834c4aefce47416655bec5
nijeysh <yejin6521@naver.com>
2024-12-12 23:10:51 +0900

    풀이중

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new StackQueueDeque().baekjoon18258();
+        new StackQueueDeque().baekjoon2164();
     }
 }
 
