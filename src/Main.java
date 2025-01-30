8b8d0419a9a3c2d8a0f5059fa83b8ee30fc974da
nijey <yejin6521@naver.com>
2025-01-30 22:54:28 +0900

    쿼드트리

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new Recursion2().baekjoon1074();
+        new Recursion2().baekjoon1992();
     }
 }
 
--- src/baekjoon/algorithm/Recursion2.java
+++ src/baekjoon/algorithm/Recursion2.java
@@ -171,8 +171,6 @@ public class Recursion2 {
 
 //        square = new int[N][N];
         path(0, 0, 0, N);
-//        System.out.println(Arrays.deepToString(square));
-//        System.out.println(square[r][c]);
     }
 
     // 가장 작은 단위로 쪼개기 (2x2)
@@ -212,6 +210,72 @@ public class Recursion2 {
         if (r >= left + size && r < left + size + size && c >= right + size && c < right + size + size) {
             path(left + size, right + size, start + (volumn * 3), size);
         }
+    }
+
+    /**
+     * 쿼드트리
+     *
+     * 흑백 영상을 압축하여 표현하는 데이터 구조로 쿼드 트리(Quad Tree)라는 방법이 있다.
+     * 흰 점을 나타내는 0과 검은 점을 나타내는 1로만 이루어진 영상(2차원 배열)에서 같은 숫자의 점들이 한 곳에 많이 몰려있으면, 쿼드 트리에서는 이를 압축하여 간단히 표현할 수 있다.
+     *
+     * 주어진 영상이 모두 0으로만 되어 있으면 압축 결과는 "0"이 되고, 모두 1로만 되어 있으면 압축 결과는 "1"이 된다.
+     * 만약 0과 1이 섞여 있으면 전체를 한 번에 나타내지를 못하고, 왼쪽 위, 오른쪽 위, 왼쪽 아래, 오른쪽 아래, 이렇게 4개의 영상으로 나누어 압축하게 되며, 이 4개의 영역을 압축한 결과를 차례대로 괄호 안에 묶어서 표현한다
+     *
+     * 위 그림에서 왼쪽의 영상은 오른쪽의 배열과 같이 숫자로 주어지며, 이 영상을 쿼드 트리 구조를 이용하여 압축하면 "(0(0011)(0(0111)01)1)"로 표현된다.
+     * N ×N 크기의 영상이 주어질 때, 이 영상을 압축한 결과를 출력하는 프로그램을 작성하시오.
+     *
+     * N 은 언제나 2의 제곱수
+     * 1 ≤ N ≤ 64
+     *
+     */
+    static char[][] quad;
+    public void baekjoon1992() throws Exception {
+        // static으로 제출
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        quad = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                quad[i][j] = str.charAt(j);
+            }
+        }
+
+        // 최초 영상이 전체를 한 번에 나타낼 수 있으면 괄호가 없어도 된다.
+        quadTree(0, 0, N);
+        bw.flush();
+        bw.close();
+    }
+
+    static void quadTree(int left,int right, int N) throws Exception {
+        if (brackets(left, right, N)) {
+            bw.write(quad[left][right]);
+        } else {
+            bw.write("(");
+            int size = N / 2;
+            for (int i = left; i <= left + size; i+=size) {
+                for (int j = right; j <= right + size; j+= size) {
+                    if (brackets(i, j, size)) {
+                        bw.write(quad[i][j]);
+                    } else {
+                        quadTree(i, j, size);
+                    }
+                }
+            }
+            bw.write(")");
+        }
+    }
+
+    static boolean brackets(int left, int right, int size) {
+        char ch = quad[left][right];
+        for (int i = left; i < left + size; i++) {
+            for (int j = right; j < right + size; j++) {
+                if (ch != quad[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     static int read() throws Exception {
