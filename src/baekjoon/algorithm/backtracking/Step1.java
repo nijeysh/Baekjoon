64247eb545190e23979b2acfa3c5fc23524ff02a
nijeysh <yejin6521@naver.com>
2025-02-17 00:13:01 +0900

    N과 M (5) - 수정

--- src/baekjoon/algorithm/backtracking/Step1.java
+++ src/baekjoon/algorithm/backtracking/Step1.java
@@ -14,38 +14,37 @@ public class Step1 {
      * N개의 자연수 중에서 M개를 고른 수열
      */
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int M;
     static int[] arr;
-    static char[] result;
+    static boolean[] bfs;
     public void baekjoon15654() throws Exception {
         int N = read();
-        int M = read();
+        M = read();
 
-        result = new char[2 * M - 1];
+        bfs = new boolean[N];
         arr = new int[N];
-        for (int i = 1; i < M; i++) {
-            result[2 * i - 1] = ' ';
-        }
         for (int i = 0; i < arr.length; i++) {
             arr[i] = read();
         }
         Arrays.sort(arr);
 
-        permutation(-1, M, 0);
+        permutation(-1, 0, "");
         bw.flush();
         bw.close();
     }
 
-    static void permutation(int curr, int M, int depth) throws Exception {
+    static void permutation(int curr, int depth, String str) throws Exception {
         if (depth == M) {
-            bw.write(result);
+            bw.write(str.trim());
             bw.newLine();
             return;
         }
 
         for (int i = 0; i < arr.length; i++) {
-            if (i != curr) {
-                result[2 * depth] = (char) (arr[i] + '0');
-                permutation(i, M, depth + 1);
+            if (!bfs[i]) {
+                bfs[i] = true;
+                permutation(i, depth + 1, str + " " + arr[i]);
+                bfs[i] = false;
             }
         }
     }
