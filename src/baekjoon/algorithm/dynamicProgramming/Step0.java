5f9633772d29dc9f35ebb294de4b9db239615b37
nijeysh <yejin6521@naver.com>
2025-02-23 20:58:36 +0900

    정수삼각형

--- src/baekjoon/algorithm/dynamicProgramming/Step0.java
+++ src/baekjoon/algorithm/dynamicProgramming/Step0.java
@@ -1,5 +1,7 @@
 package baekjoon.algorithm.dynamicProgramming;
 
+import java.util.Arrays;
+
 public class Step0 {
 
     /**
@@ -38,6 +40,61 @@ public class Step0 {
 
     }
 
+    /**
+     * 정수 삼각형
+     */
+    static int[][] triangle;
+    static int N;
+    public void baekjoon1932() throws Exception {
+        N = read();
+        triangle = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = read();
+            }
+        }
+
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+
+        System.out.println(triangle[0][0]);
+    }
+
+    static void triangle(int parent, int depth) throws Exception {
+//        if (depth == N) {
+//            int val = dp[parent];
+//            if (val > max) {
+//                max = val;
+//            }
+////            System.out.println(dp[parent]);
+////            System.out.println(Arrays.toString(dp));
+//            return;
+//        }
+//        dp[0] = arr[0];
+//
+//        dp[1] = arr[0] + arr[1];
+//        dp[2] = arr[0] + arr[2];
+//
+//        dp[3] = arr[0] + arr[1] + arr[3];
+//        dp[4] = arr[0] + arr[1] + arr[4];
+//        dp[4] = arr[0] + arr[2] + arr[4];
+//        dp[5] = arr[0] + arr[2] + arr[5];
+
+        // parent = 0, start = 1, end = 2
+        // parent = 1, start = 3, end = 4
+//        int start = parent + depth;
+//        int end = start + 1;
+//        for (int i = start; i <= end; i++) {
+//            dp[i] = dp[parent] + arr[i];
+//            System.out.println("dp[" + i + "] = dp[" + parent + "] + arr[" + i + "];");
+////            System.out.println(Arrays.toString(dp));
+//            triangle(i, depth + 1);
+//        }
+    }
 
     private static int read() throws Exception {
         int n;
